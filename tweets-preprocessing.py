e2efc2e8d8f1ed3d5b97271f4ef1cdf56b3628de
Basma Kadry <basma.adrii@gmail.com>
2017-12-17 20:15:56 +0200

    Edit Tweets Preprocessing

--- tweets-preprocessing.py
+++ tweets-preprocessing.py
@@ -1,10 +1,16 @@
 import preprocessor as p
 from nltk.corpus import stopwords
+from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.stem import PorterStemmer
+from nltk.tokenize import RegexpTokenizer
 import string
-import numpy as np
 
 
 def filter_tweets():
+
+    lmtzr = WordNetLemmatizer()
+    ps = PorterStemmer()
+    tokenizer = RegexpTokenizer(r'\w+')
 
     with open('./train_semeval2018task2/train_semeval2018task2/crawler/data/tweet_by_ID_12_12_2017__11_01_28.txt.text') as f:
         tweets = f.readlines()
@@ -17,8 +23,11 @@ def filter_tweets():
         print tweets[i]
         tweets[i] = p.clean(tweets[i])
         tweets[i] = filter(lambda x: x in set(string.printable), tweets[i])
-        tweets[i] = tweets[i].encode().split()
-        tweets[i] = [word for word in tweets[i] if word not in stopwords.words('english')]
+        tweets[i] = tokenizer.tokenize(tweets[i].encode())
+        tweets[i] = [word.lower() for word in tweets[i] if word.lower() not in stopwords.words('english')]
+        tweets[i] = [lmtzr.lemmatize(word) for word in tweets[i]]
+        tweets[i] = [lmtzr.lemmatize(word, 'v') for word in tweets[i]]
+
         print 'after:'
         print tweets[i]
         for w in tweets[i]:
